50bc99968132debfb7fbb498cdc1704643cca343
TagiaNetwork <65549779+TagiaNetwork@users.noreply.github.com>
2021-02-07 20:01:20 -0600

    Add files via upload

--- Open Carnage/WeaponWeight.lua
+++ Open Carnage/WeaponWeight.lua
@@ -1,75 +1,125 @@
--- Weapon Weight
--- Version: 0.3
--- Uplodaed: OpenCarnage.net
--- Credits:
-	-- Snagged the weapon tags from Backpack weapons by aLTis (https://opencarnage.net/index.php?/profile/1639-altis/)
-	-- SetSpeedOfPlayer function from sprint script by Kavawuvi (https://opencarnage.net/index.php?/profile/1240-kavawuvi/)
-	-- Tucker for OpenCarnage forums
-
---------------------
--- Configuration: --
---------------------
-
-local enabled = true
-local player_weight = 1 -- how heavy the player is by default
-
---------------------
+--[[
+  Title: Weapon Weight
+  Version: 1.0
+  Description: Add "weight" to each weapon to slow or speed up a player's movement.
+  Credits: aLTis (https://opencarnage.net/index.php?/profile/1639-altis/), Kavawuvi (https://opencarnage.net/index.php?/profile/1240-kavawuvi/), and Chalwk (https://opencarnage.net/index.php?/profile/1539-chalwk/)
+  Really special thanks to Chawlk, his script organizational structure helped me clean things up a lot. Check out his GitHub if you want more great scrips (https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS)!
+]]--
 
 api_version = "1.12.0.0"
-ce, client_info_size = 0x40, 0xEC
 
 
-local WEAPONS = {
-    ["weapons\\assault rifle\\assault rifle"] = {["name"] = "ar", ["weight"] = "0.2"},
-    ["weapons\\shotgun\\shotgun"] = {["name"] = "sg", ["weight"] = "0.3"},
-    ["weapons\\sniper rifle\\sniper rifle"] = {["name"] = "sr", ["weight"] = "0.2"},
-    ["weapons\\pistol\\pistol"] = {["name"] = "p", ["weight"] = "0"},
-    ["weapons\\plasma pistol\\plasma pistol"] = {["name"] = "pp", ["weight"] = "0"},
-    ["weapons\\needler\\mp_needler"] = {["name"] = "n", ["weight"] = "0.2"},
-    ["weapons\\plasma rifle\\plasma rifle"] = {["name"] = "pr", ["weight"] = "0.2"},
-    ["weapons\\rocket launcher\\rocket launcher"] = {["name"] = "rl", ["weight"] = "0.3"},
-    ["weapons\\flamethrower\\flamethrower"] = {["name"] = "f", ["weight"] = "0"},
-    ["weapons\\plasma_cannon\\plasma_cannon"] = {["name"] = "frg", ["weight"] = "0.3"},
-    ["weapons\\gravity rifle\\gravity rifle"] = {["name"] = "ar", ["weight"] = "0.2"},
+local WeaponWeight = {
+  version = 1.0,
+
+  enabled = true,
+
+  -- Avaliable Modes:
+  -- 0 = Weight is calculated based on the total weight of weapons in the player's inventory.
+  -- 1 = Weight is calculated based on the currently held weapon.
+  mode = 0,
+
+  -- Default weight of the player.
+  default_weight = 1,
+
+  weapons = {
+    ["weapons\\assault rifle\\assault rifle"] = 0,
+    ["weapons\\shotgun\\shotgun"] = 0,
+    ["weapons\\sniper rifle\\sniper rifle"] = 0,
+    ["weapons\\pistol\\pistol"] = 0,
+    ["weapons\\plasma pistol\\plasma pistol"] = 0,
+    ["weapons\\needler\\mp_needler"] = 0,
+    ["weapons\\plasma rifle\\plasma rifle"] = 0,
+    ["weapons\\rocket launcher\\rocket launcher"] = 0,
+    ["weapons\\flamethrower\\flamethrower"] = -0.3,
+    ["weapons\\plasma_cannon\\plasma_cannon"] = 0,
+    ["weapons\\gravity rifle\\gravity rifle"] = -2,
+  }
 }
 
 
 function OnScriptLoad()
-    if enabled == true then
-        register_callback(cb["EVENT_JOIN"],"OnPlayerJoin")
-        register_callback(cb["EVENT_LEAVE"],"OnPlayerLeave")
-        register_callback(cb["EVENT_SPAWN"],"OnPlayerSpawn")
-        register_callback(cb["EVENT_VEHICLE_ENTER"],"OnVehicleEnter")
-        register_callback(cb["EVENT_VEHICLE_EXIT"],"OnVehicleExit")
-        register_callback(cb["EVENT_TICK"],"OnTick")
-    end
-    if halo_type == "PC" then execute_command("quit")end --play custom edition lul
+  WeaponWeight:Load()
 end
 function OnScriptUnload() end
 
-inVehicle = {}
 
-function OnPlayerJoin(PlayerIndex)
-    inVehicle[PlayerIndex] = false
+function WeaponWeight:Load()
+  self.players = {  }
+
+  if (self.enabled) then
+    register_callback(cb["EVENT_JOIN"],"OnPlayerConnect")
+    register_callback(cb["EVENT_LEAVE"],"OnPlayerDisconnect")
+
+    register_callback(cb["EVENT_TICK"],"OnTick")
+
+    -- Please use Halo: Custom Edition, it's better.
+    if (halo_type == "PC") then
+      execute_command("quit")
+    end
+  end
 end
 
-function OnPlayerLeave(PlayerIndex)
-    inVehicle[PlayerIndex] = false
+
+function WeaponWeight:InitPlayer(PlayerIndex, Reset)
+  if (not Reset) then
+    self.players[PlayerIndex] = {
+      weight = self.default_weight,
+      inventory = {
+        ["slot1"] = "None",
+        ["slot2"] = "None",
+        ["slot3"] = "None"
+      },
+      vehicle = false
+    }
+  else
+    self.players[PlayerIndex] = nil
+  end
 end
 
-function OnVehicleEnter(PlayerIndex)
-    inVehicle[PlayerIndex] = true
+
+function OnPlayerConnect(PlayerIndex)
+  WeaponWeight:InitPlayer(PlayerIndex, false)
 end
 
-function OnVehicleExit(PlayerIndex)
-    inVehicle[PlayerIndex] = false
+
+function OnPlayerDisconnect(PlayerIndex)
+  WeaponWeight:InitPlayer(PlayerIndex, true)
 end
 
-function OnPlayerSpawn(PlayerIndex)
-    SetSpeedOfPlayer(PlayerIndex, player_weight)
+
+
+
+function OnTick()
+  for PlayerIndex = 1, 16 do
+    if (player_present(PlayerIndex) and player_alive(PlayerIndex)) then
+      local player = get_dynamic_player(PlayerIndex)
+      local player_weight = WeaponWeight.players[PlayerIndex]["weight"]
+
+      local weapons = WeaponWeight.weapons
+      local inventory = WeaponWeight.players[PlayerIndex]["inventory"]
+
+      for i = 1, 4 do
+        inventory["slot" .. i] = get_object_memory(read_dword(player + 0x2F8 + 4*(i-1)))
+        GetSlot(PlayerIndex, "slot" .. i, GetName(inventory["slot" .. i]))
+        if (inventory["slot" .. i] ~= nil and weapons[GetName(inventory["slot" .. i])] ~= nil) then
+          player_weight = player_weight - weapons[GetName(inventory["slot" .. i])]
+        end
+      end
+
+      SetSpeedOfPlayer(PlayerIndex, player_weight)
+    end
+  end
 end
 
-function SetSpeedOfPlayer(PlayerIndex,Speed)
+
+function GetSlot(PlayerIndex, Slot, Item)
+  WeaponWeight.players[PlayerIndex][Slot] = Item
+end
+
+
+
+function SetSpeedOfPlayer(PlayerIndex, Speed)
     Speed = math.floor(Speed * 40 + 0.5) / 40
     local player = get_player(PlayerIndex)
     local player_speed = (read_float(get_player(PlayerIndex)) * 40 + 0.5) / 40
@@ -77,40 +127,10 @@ function SetSpeedOfPlayer(PlayerIndex,Speed)
         write_float(get_player(PlayerIndex) + 0x6C, Speed)
     end
 end
+
+
 function GetName(object)
-    if object ~= 0 then
+  if (object ~= 0 and object ~= nil ) then
         return read_string(read_dword(read_short(object) * 32 + 0x40440038))
-    end
-end
-
-
-
-function OnTick()
-    for PlayerIndex = 1,16 do
-        if(player_alive(PlayerIndex) == true) then
-            local player = get_dynamic_player(PlayerIndex)
-            if player ~= nil and read_float(player + 0xE0) ~= 0 then
-                local weapon_slot = read_byte(player + 0x2F2)
-                local primary_weapon = get_object_memory(read_dword(player + 0x2F8 + 4*0))
-                local secondary_weapon = get_object_memory(read_dword(player + 0x2F8 + 4*1))
-
-                local primary_weapon_name = GetName(primary_weapon)
-                local secondary_weapon_name = GetName(secondary_weapon)
-                local weight = 0
-
-                if primary_weapon_name ~= nil and lookup_tag("weap", primary_weapon_name) ~= nil and WEAPONS[primary_weapon_name]["name"] ~= nil then
-                    weight = weight + WEAPONS[primary_weapon_name]["weight"]
-                end
-
-                if  secondary_weapon_name ~= nil and lookup_tag("weap", secondary_weapon_name) ~= nil and WEAPONS[secondary_weapon_name]["name"] ~= nil then
-                    weight = weight + WEAPONS[secondary_weapon_name]["weight"]
-                end
-
-                if inVehicle[PlayerIndex] == false then
-                    SetSpeedOfPlayer(PlayerIndex,player_weight - weight)
-                end
-
-            end
-        end
     end
 end
